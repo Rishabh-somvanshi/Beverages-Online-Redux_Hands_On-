51ca821d9e0118a834ac033f58e592d6b937b8c8
Rishabh-somvanshi <rishabhsomvanshi87@gmail.com>
2021-08-18 13:20:58 +0530

    2 files modified, 2 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './style.css';
 import Login from './Login';
+import NavBar from './Navbar';
 
 export default function App() {
   return (
     <div>
       <p class="pageHeader">Beverages Online</p>
-
+      <div>
+        <NavBar />
+      </div>
       <div>
         <Login />
       </div>
